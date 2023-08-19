b383824ceba455e1418729c394b2d18a7734a618
jqAguilera <jq.aguilera@outlook.com>
2023-08-18 20:04:12 -0400

    Create Matriz.java

---
+++ Matriz/src/main/java/com/jqaguilera/matriz/Matriz.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jqaguilera.matriz;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jqagu
+ */
+public class Matriz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
