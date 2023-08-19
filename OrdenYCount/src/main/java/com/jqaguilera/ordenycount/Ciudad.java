2a8723eb0deed892e17607282ac96efe55990c97
jqAguilera <jq.aguilera@outlook.com>
2023-08-18 20:02:40 -0400

    Create Ciudad.java

---
+++ OrdenYCount/src/main/java/com/jqaguilera/ordenycount/Ciudad.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jqaguilera.ordenycount;
+
+/**
+ *
+ * @author jqagu
+ */
+public class Ciudad {
+    
+    String nombre;
+    int cantidad;
+
+    public Ciudad() {
+    }
+
+    public Ciudad(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+}
